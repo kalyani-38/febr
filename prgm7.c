28d2a73bee23092c284d9b60b6032c544f95f906
kalyani <kalyani.y@thundersoft.com>
2022-02-17 21:46:02 +0530

    feb17

---
+++ prgm7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter the number ");
+    scanf("%d",&num);
+    
+   if(num%2==0)
+    printf("number is even %d",num);
+     else 
+    printf("number is odd %d",num);
+
+
+    return 0;
+}
+
